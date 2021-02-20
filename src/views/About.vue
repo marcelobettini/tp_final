c443fd3393d3ec5d4dfd5ac040952ebe6302142f
micaperone <micaperone@gmail.com>
2021-02-19 21:55:00 -0300

    agregue presentacion

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,63 @@
 <template>
   <div class="about">
-    <h1>Acá desarrolla Micaela</h1>
+    <div class="container-fluid ">
+    <div class="row">
+        <div class="col-12 col-lg-4 p-4 ">
+            <img src="../assets/LuzAdentro/logo.png" alt="Logo de Luz Adentro" class= "w-100">
+        </div>
+        <div class="col-12 col-lg-8 p-3">
+            <h1>¿Quienes somos?</h1>
+            <p> Somos una marca de billeteras y accesorios para efectos personales del día a día. 
+                Diseñamos productos que utilizan polietileno descartado (sachets de leche) en su interior
+                para impermeabilizar y dar estructura.</p>
+            <p> Proponemos un vínculo entre las personas y sus productos, que
+                sirva para cuestionar profundamente el origen de cada
+                compra y que los mueva a tomar decisiones cada vez más
+                concientes. Buscamos resignificar el concepto de material de
+                desperdicio, interviniendo la cadena de consumo cuando un
+                componente termina su función primaria y se convierte en
+                “basura”.</p>
+            <p> Recuperamos material de desecho otorgándole utilidad, para
+                rescatarlo de rellenos sanitarios y océanos. Fabricamos productos
+                que se puedan usar y apreciar por el mayor tiempo posible, 
+                para evitar que terminen desechados</p>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-12 "> <!--Mision-->
+        <h3 class="amarillo">MISIÓN</h3>
+        <p>"Creamos billeteras y accesorios que optimizan el transporte de efectos personales, en día día. Diseñamos, utilizando para impermeabilizar su interior, material descartable para dar estructura. En toda la gestión, estamos atentos al gasto energético y la generación de residuos y nuevamente hacernoos cargo de ellos, reciclándolos. Imaginamos un vínculo entre personas y nuestros productos, que sirva para cuestionar profundamente el origen de cada cada movimiento en éste sistema, tomar decisiones cada vez más conscientes dentro de la cadena mueva de consumo de la que todos somos parte</p>
+        </div>
+        <div class="col-12 "><!--Vision-->
+        <h3 class="amarillo">VISIÓN</h3>
+        <p>Queremos resignificar el concepto de material de desperdicio. Intervenimos la cadena de consumo cuando un componente termina su función primaria se convierte "basura". Recuperamos material de desecho otorgándole utilidad, para rescatarlo de rellenos sanitarios y océanos. Pensamos el viaje de los productos que elegimos antes de que lleguen a nuestras manos, es una llamada a la acción que transforma profundamente nuestra relación con actos individuales y entorno. Reconocernos como seres activos fundamentales en la construcción colectiva humana. Fabricamos productos que se puedan usar y apreciar por el mayor tiempo posible, para evitar que terminen desechados.</p>
+        </div>
+        <div class="col-12"><!--Valores-->
+        <h3 class="amarillo">VALORES</h3>
+        <p>Producción ética para la responsabilidad, que cultive el valor por los productos de origen autogestivo. Diseño consciente - mayor coherencia. Gestar tendencias que estimulen una cultura material comprometida con el planeta y las personas. Promovemos vínculos entre productores y consumidores más cálidos, que impulsen nuevas formas de  ofrecer productos de calidad y con garantía, como forma de economía la espiral de comprar, tirar Los principios comprar. cotidianas, Cradle guían nuestra toma de decisiones de nuestro impacto en nos llevan a buscar una comprensión más profunda entorno cómo reducirlo.</p>
+        </div>
+
+    </div>
+    <div class="row  ">
+      <div class="col"><h3 class="amarillo text-center">SECUENCIA DE RECICLADO</h3></div>
+    </div>
+    <div class="row p-4"> 
+        <div class="col-12 col-lg-4 ">
+            <img src="../assets/LuzAdentro/secuencia1.jpg" alt="Secuencia de armado" class="w-100">
+        </div>
+        <div class="col-12 col-lg-4">
+            <img src="../assets/LuzAdentro/secuencia2.jpg" alt="Secuencia de armado" class="w-100">
+        </div>
+        <div class="col-12 col-lg-4">
+            <img src="../assets/LuzAdentro/secuencia3.jpg" alt="Secuencia de armado" class="w-100">
+        </div>
+    </div>
+    
+</div>
+
   </div>
 </template>
+
+
+
